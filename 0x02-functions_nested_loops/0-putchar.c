f0b97e1f36206bad176aec5b4496d8e4b9460bfb
IMANE EL MARGHANI <96700925+ImaneELMARGHANI@users.noreply.github.com>
2023-08-17 22:37:40 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,19 +12,3 @@ int main(void)
 		write(1, "_putchar\n", 9);
 			return (0);
 }
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-/**
- *  * main - Entry point
- *   * Description: prints "_putchar" to stdrout
- *    * Return: Always (0)
- *     */
-
-int main(void)
-{
-		write(1, "_putchar\n", 9);
-			return (0);
-}
->>>>>>> ea996e95363fb06f41470fd99c6a3659caee9238
