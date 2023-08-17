d179ce18b68ff05340b773a3a6117487f0c778a5
IMANE EL MARGHANI <96700925+ImaneELMARGHANI@users.noreply.github.com>
2023-08-17 16:39:15 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,7 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
+int _islower(char c);
 int _isalpha(int c);
 void print_to_98(int n);
 int add(int, int);
