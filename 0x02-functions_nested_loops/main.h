e76a4bdec8d4bb9fc70172d672e073f574b01275
IMANE EL MARGHANI <96700925+ImaneELMARGHANI@users.noreply.github.com>
2023-08-17 17:09:31 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,17 +2,18 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char c);
+void _putchar(int c);
 void print_alphabet(void);
-void print_alphabet_x10(void);
+void print_alphabet_x10();
 int _islower(char c);
 int _isalpha(int c);
-void print_to_98(int n);
-int add(int, int);
-void times_table(void);
+int print_sign(int x);
+int _abs(int x);
+int print_last_digit(int x);
 void jack_bauer(void);
-int print_last_digit(int n);
-int _abs(int);
-int print_sign(int n);
+void times_table(void);
+int add(int x, int y);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
