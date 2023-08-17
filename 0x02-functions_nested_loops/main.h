d421665489342e6137b90a7f87e934a73581ff31
ImaneELMARGHANI <imanemarghani@gmail.com>
2023-08-17 07:01:07 -0700

    ?!

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H_
+
+void _putchar(int c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10();
+
+int _islower(char c);
+
+int _isalpha(int c);
+
+int print_sign(int x);
+
+int _abs(int x);
+
+int print_last_digit(int x);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int x, int y);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif
