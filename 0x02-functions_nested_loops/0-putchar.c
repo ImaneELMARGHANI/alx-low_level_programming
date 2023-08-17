98661e1d612697f59f2707d2f31855a2ebb86d60
IMANE EL MARGHANI <96700925+ImaneELMARGHANI@users.noreply.github.com>
2023-08-17 15:54:14 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,24 +12,3 @@ int main(void)
 		write(1, "_putchar\n", 9);
 			return (0);
 }
-=======
-#include "main.h"
-#include <stdio.h>
-
-/**
- * print_alphabet - function
- * Description: takes nothing as arguments, prints the alphabet in lowercase
- */
-
-void print_alphabet(void)
-{
-	int i = 97;
-
-	while (i < 123)
-	{
-		_putchar(i);
-		i++;
-	}
-	_putchar('\n');
-}
->>>>>>> 6b1991c023014b1a82ec10e43cfa9f3d97f427c1
